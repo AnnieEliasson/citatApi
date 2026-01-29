38adeccecf6fde1d709b45493fd469f371d674cb
Annie Eliasson <annie.eliasson@gmail.com>
2026-01-29 11:15:49 +0100

    first

---
+++ Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+// Lägg till den DbContext ni skapar för er databas här
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=app.db"));
+
+//Låt detta vara kvar! Utan denna inställning kommer inte websidan att få access till API:et.
+// Läs mer här: https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
+
+
+    List<string> quotes = File.ReadAllLines("Data/quotes.txt").ToList();
+
+    if (!db.Quotes.Any())
+    {
+        db.Quotes.AddRange(
+            quotes.Select(q => new Quote { Text = q })
+        );
+
+        db.SaveChanges();
+    }
+}
+
+// Denna hör ihop med CORS-inställningen ovan
+app.UseCors();
+
+//Ni ska inte skriva era endpoints här i Program.cs utan i separata controllers, så använd denna:
+app.MapControllers();
+
+app.Run();
+
+
+public class Quote
+{
+    public int Id { get; set; }
+    public required string Text { get; set; }
+}
---
+++ QuoteController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[ApiController]
+[Route("/api/quote")]
+public class QuoteController(AppDbContext _db) : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<ActionResult<string>> GetQuote()
+    {
+        int qoutesLength = await _db.Quotes.CountAsync();
+        int randomId = Random.Shared.Next(1, qoutesLength);
+
+        Quote? quote = await _db.Quotes.FirstOrDefaultAsync((q) => q.Id == randomId);
+
+
+        if (quote is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(quote.Text);
+    }
+
+}
